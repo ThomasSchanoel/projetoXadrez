2e029a92a01872449925043f7c6547759cdaddb7
Thomas Schanoel <schanoel500@gmail.com>
2024-02-22 20:44:13 -0300

    Implementando as restricoes de movimento

--- ProjetoXadrez/Tabuleiro/Peca.cs
+++ ProjetoXadrez/Tabuleiro/Peca.cs
@@ -24,6 +24,27 @@ namespace tabuleiro
             qtdMovimentos++;
         }
 
+        public bool existeMovimentosPossiveis()
+        {
+            bool[,] mat = movimentosPossiveis();
+            for (int i = 0; i < tab.linhas; i++)
+            {
+                for (int j = 0; j < tab.colunas; j++)
+                {
+                    if (mat[i,j] == true)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool podeMoverPara(Posicao pos)
+        {
+            return movimentosPossiveis()[pos.linha, pos.coluna];
+        }
+
         public abstract bool[,] movimentosPossiveis();
 
 
